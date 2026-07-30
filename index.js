28b99e262f2f5eb118b81d702d1d44885a1b3a17
abhinavbca <dabhinav681@gmail.com>
2026-07-30 09:34:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,34 +6,89 @@ function Recipe()
 
     return(
 
-        <div className="Recipe">
+        <div className="container">
+           <div className="row">
+   <div className="col-4">
 
             {/* Card 1 */}
+<div className="card" style={{width: "18rem;"}}>
+  <img className="card-img-top" src="image1.jpg" alt="CHICKEN NOODLES SOUP"/>
+  <div className="card-body">
+    <div className="card-body">
+  <h5 className="card-title">CHICKEN NOODLE SOUP</h5>
+
+  <ul className="list-group list-group-flush"></ul>
+     <ul className="list-group list-group-flush">
+    <li className="list-group-item">2 Tablespoons extra virgin olive oil</li>
+    <li className="list-group-item">1 yellow onion</li>
+    <li className="list-group-item">2 medium carrots</li>
+    <li className="list-group-item">2 stalk celery</li>
+    <li className="list-group-item">8 cups chiken broth</li>
+    <li className="list-group-item">1(16 oz) package wide egg noodles</li>
+    <li className="list-group-item">1 cup cooked chiken</li>
+  </ul>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+
+</div></div>
 
 
 
+<div className="col-4">
+
+<div className="card" style={{width: "18rem;"}}>
+  <img className="card-img-top" src="image2.jpg" alt="CURRIED EGG SALAD"/>
+  <div className="card-body">
+    <div className="card-body">
+  <h5 className="card-title">CURRIED EGG SALAD</h5>
+
+  <ul className="list-group list-group-flush"></ul>
+     <ul className="list-group list-group-flush">
+    <li className="list-group-item">12 hard boiled eggs</li>
+    <li className="list-group-item">1/2</li>
+    <li className="list-group-item">2 medium carrots</li>
+    <li className="list-group-item">2 stalk celery</li>
+    <li className="list-group-item">8 cups chiken broth</li>
+    <li className="list-group-item">1(16 oz) package wide egg noodles</li>
+    <li className="list-group-item">1 cup cooked chiken</li>
+  </ul>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+</div>
 
 
 
-            {/* Card 2 */}
-
-
-
-
-
+<div className="col-4">
 
             {/* Card 3 */}
+<div className="card" style={{width: "18rem;"}}>
+  <img className="card-img-top" src="image3.jpg" alt="OAT CLUSTERS"/>
+  <div className="card-body">
+    <div className="card-body">
+  <h5 className="card-title">OAT CLUSTERS</h5>
 
+  <ul className="list-group list-group-flush"></ul>
+     <ul className="list-group list-group-flush">
+    <li className="list-group-item">12 hard boiled eggs</li>
+    <li className="list-group-item">1/2</li>
+    <li className="list-group-item">2 medium carrots</li>
+    <li className="list-group-item">2 stalk celery</li>
+    <li className="list-group-item">8 cups chiken broth</li>
+    <li className="list-group-item">1(16 oz) package wide egg noodles</li>
+    <li className="list-group-item">1 cup cooked chiken</li>
+  </ul>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
 
-
-
-
-
-        </div>
+</div></div></div></div>
 
     );
 
-}
+  }
 
 // TODO 2:
 // Create the React Root
